ea6145718a0107adc8c88f05b177e702826d05f6
MaDaLla <mdllanes80@gmail.com>
2024-05-27 03:42:21 -0300

    coreccion de js

--- js/main.js
+++ js/main.js
@@ -69,7 +69,9 @@ function validar() {
     abrirPropiedad(user);
 }
 //asignando un listener al boton submit del fomulario
-document.querySelector(".button_publicar a").addEventListener('click', validar);
+if (document.querySelector(".button_publicar a") !== null) {
+    document.querySelector(".button_publicar a").addEventListener('click', validar)
+};
 //cambiar lo de arriba para que se pueda mantener el js
 
 
@@ -261,42 +263,43 @@ if (!(form_usuario === null && form_prop === null)) {
 
 /*Contacto*/
 //Scripts js
-
-document.querySelector('.contact_form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    validarFormulario();
-});
-
-function validarFormulario() {
-    const nombre = document.querySelector('#nombre_contacto').value;
-    const email = document.querySelector('#email_contacto').value;
-    const telefono = document.querySelector('#phone_contacto').value;
-    const mensaje = document.querySelector('#comentarios_contacto').value;
-    const contacto = document.querySelector('input[name="contacto"]:checked');
-    const servicio = document.querySelector('#motivo').value;
-
-    // Eliminar clases de error anteriores
-    document.querySelectorAll('.error').forEach(element => {
-        element.classList.remove('error');
+if (document.querySelector('.contact_form') !== null) {
+    document.querySelector('.contact_form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        validarFormulario();
     });
 
-    // Comprobamos si alguno está vacío
-    if (!nombre || !email || !telefono || !mensaje || !contacto || !servicio) {
-        // Marcamos con error los vacíos
-        if (!nombre) document.querySelector('#nombre_contacto').classList.add('error');
-        if (!email) document.querySelector('#email_contacto').classList.add('error');
-        if (!telefono) document.querySelector('#phone_contacto').classList.add('error');
-        if (!mensaje) document.querySelector('#comentarios_contacto').classList.add('error');
-        if (!contacto) {
-            document.querySelectorAll('input[name="contacto"]').forEach(button => {
-                button.nextElementSibling.classList.add('error'); // Marcar el label asociado al radio button
-            });
+    function validarFormulario() {
+        const nombre = document.querySelector('#nombre_contacto').value;
+        const email = document.querySelector('#email_contacto').value;
+        const telefono = document.querySelector('#phone_contacto').value;
+        const mensaje = document.querySelector('#comentarios_contacto').value;
+        const contacto = document.querySelector('input[name="contacto"]:checked');
+        const servicio = document.querySelector('#motivo').value;
+
+        // Eliminar clases de error anteriores
+        document.querySelectorAll('.error').forEach(element => {
+            element.classList.remove('error');
+        });
+
+        // Comprobamos si alguno está vacío
+        if (!nombre || !email || !telefono || !mensaje || !contacto || !servicio) {
+            // Marcamos con error los vacíos
+            if (!nombre) document.querySelector('#nombre_contacto').classList.add('error');
+            if (!email) document.querySelector('#email_contacto').classList.add('error');
+            if (!telefono) document.querySelector('#phone_contacto').classList.add('error');
+            if (!mensaje) document.querySelector('#comentarios_contacto').classList.add('error');
+            if (!contacto) {
+                document.querySelectorAll('input[name="contacto"]').forEach(button => {
+                    button.nextElementSibling.classList.add('error'); // Marcar el label asociado al radio button
+                });
+            }
+            if (!servicio) document.querySelector('#motivo').classList.add('error');
+            alert("Debe completar todos los campos para poder comunicarnos con usted");
+        } else {
+            console.log("llego a todo esta bien");
+            alert("Formulario enviado con exito");
         }
-        if (!servicio) document.querySelector('#motivo').classList.add('error');
-        alert("Debe completar todos los campos para poder comunicarnos con usted");
-    } else {
-        console.log("llego a todo esta bien");
-        alert("Formulario enviado con exito");
     }
 }
 
