9ba98a0a808897f4c2bd97fc164489a689efece0
Marcelo <mdl80@gmail.com>
2024-07-01 18:41:21 -0300

    se añaden los cambios de Flavio

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 from app.database import init_app
 import os
 
-template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = (os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'app', 'templates')
+static_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app', 'static')
 
-app=Flask(__name__, template_folder=template_dir)
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 init_app(app)
 
