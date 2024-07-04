80261ee330df14d76a0a744db9a8a37e439c7f29
Marcelo <mdl80@gmail.com>
2024-07-04 07:59:27 -0300

    se corrijen errores de sintaxys en sentencias sql

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from app.database import get_db
 
 class User:
     
-    def __init__(self, idUsuario=None, nombre_apellido=None, DNI=None, direccion=None, codigo_postal=None,email=None, password=None, razon_social=None, matricula=None, idRol=None):
-        self.idUsuario = idUsuario
+    def __init__(self,nombre_apellido, DNI, direccion, codigo_postal,email, password, razon_social='', matricula='', idRol=None):
+        self.idUsuario = None
         self.nombre_apellido = nombre_apellido
         self.DNI = DNI
         self.direccion = direccion
@@ -19,12 +19,12 @@ class User:
         db = get_db()
         cursor = db.cursor()
         if self.idUsuario:
-            cursor.execute("""UPDATE Usuario SET nombre_apellido = %s, DNI = %s, direccion = %s, codigo_postal = %s, email = %s, password = %s, razon_social = %s, matricula = %s, idRol = %s 
-                WHERE idUsuario = %s""",
+            cursor.execute("""UPDATE Usuario SET nombre_apellido = ?, DNI = ?, direccion = ?, codigo_postal = ?, email = ?, password = ?, razon_social = ?, matricula = ?, idRol = ? 
+                WHERE idUsuario = ?""",
                 (self.nombre_apellido, self.DNI, self.direccion, self.codigo_postal, self.email, self.password, self.razon_social, self.matricula, self.idRol))
         else:
             cursor.execute("""
-                INSERT INTO Usuario (nombre_apellido, DNI, direccion, codigo_postal, password, razon_social, matricula, irRol) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", 
+                INSERT INTO Usuario (nombre_apellido, DNI, direccion, codigo_postal, password, razon_social, matricula, irRol) VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", 
                 (self.nombre_apellido, self.DNI, self.direccion, self.codigo_postal, self.email, self.password, self.razon_social, self.matricula, self.idRol))
             self.idUsuario = cursor.lastrowid
         db.commit()
@@ -33,7 +33,7 @@ class User:
     def delete(self):
         db = get_db()
         cursor = db.cursor()
-        cursor.execute("DELETE FROM Usuario WHERE idUsuario = %s", (self.idUsuario,))
+        cursor.execute("DELETE FROM Usuario WHERE idUsuario = ?", (self.idUsuario,))
         db.commit()
         cursor.close()
 
@@ -50,7 +50,7 @@ class User:
     def get_by_id(idUsuario):
         db = get_db()
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM Usuario WHERE idUsuario = %s", (idUsuario,))
+        cursor.execute("SELECT * FROM Usuario WHERE idUsuario = ?", (idUsuario,))
         user = cursor.fetchone()
         cursor.close()
         return user
