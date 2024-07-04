fd6266787e68268bb9de7152efb102d2ee88b3d3
Marcelo <mdl80@gmail.com>
2024-07-03 22:15:55 -0300

    agregados metodos a test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Flask, jsonify, render_template, render_template_string, request
+from flask import Flask, Response, jsonify, render_template, render_template_string, request
 from app import models
 from app.database import init_app
 import os
@@ -34,24 +34,32 @@ def recibir():
 @app.route('/sesion')
 def inicioSesion():
     return render_template('inicio_sesion.html')
-#api de prueba para perfil FUNCIONA (Falta js para cargar datos)
+
+#api de prueba para perfil recibe el id de usuario y envia los datos para que lo cargeu la vista
 @app.route('/perfil')
 def perfil():
     return render_template('perfil.html')
 
+#api que recibe datos de usuario y contraseña devuelve true o false y lo devuelve al front
 @app.route('/auth', methods=['POST'])
 def autorizacion():
     data = request.get_json()
     if not data:
-        return jsonify({'error':'No se recibieron datos'}),400
-    mail = data.get('email')
+        return jsonify({'error': 'No se recibieron datos'}), 400
+    
+    email = data.get('email')
     password = data.get('password')
-    validado = models.User.authLoguin(mail,password)
-    if(validado):
-        return jsonify({'validado':True}),200
+    
+    if not email or not password:
+        return jsonify({'error': 'Email y contraseña son requeridos'}), 400
+    
+    idUsuario = models.User.authLoguin(email, password)
+    print('el id de usuario es:' + str(idUsuario))
+    
+    if idUsuario >= 1:
+        return jsonify({'validado': True, 'idUsuario': idUsuario, 'email': email}), 200
     else:
-        return jsonify({'validado':False}),401
-
+        return jsonify({'validado': False, 'idUsuario': -1}), 401
     
 if __name__== '__main__':
     app.run(debug=True)
